a6c9d50df2ee6830d68172d7e8e69f40bd95761c
oscar solano <zedekisa555@gmail.com>
2024-06-13 13:40:45 -0500

    commit mas targetas

--- app/src/main/java/com/example/givinglandv1/MainViewModel.kt
+++ app/src/main/java/com/example/givinglandv1/MainViewModel.kt
@@ -27,16 +27,184 @@ class MainViewModel : ViewModel() {
                 location = "Popayán",
                 isFavorite = false
             ),
+
             Item(
                 images = listOf(
                     Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
                     Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
                     Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
                 ),
-                title = "auto",
+                title = "Camiseta",
+                description = "Esta en excelente estado,talla s",
+                additionalInfo = "Intercambio",
+                location = "Cali",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Zapatos para hombre",
+                description = "Talla 38 estan en excelente estado,poco uso",
+                additionalInfo = "Intercambio",
+                location = "Manizales",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Portatil",
+                description = "Esta funcionando perfectamente,poco uso",
+                additionalInfo = "Intercambio",
+                location = "Bogota",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Pantalon para dama",
+                description = "Esta nuevo,lo pedi pero me quedo muy exacto",
+                additionalInfo = "Intercambio",
+                location = "Buga",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Huawei P30",
+                description = "Lo entrego con sus accesorios,esta bueno",
+                additionalInfo = "Intercambio",
+                location = "Armenia",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Camiseta Hombre",
+                description = "Marca polo,solo tiene 2 puestas",
+                additionalInfo = "Intercambio",
+                location = "Pasto",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Bolso",
+                description = "Bolso grande para viaje marca TOTTO",
+                additionalInfo = "Intercambio",
+                location = "Antioquia",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Cable carga",
+                description = "Se intercambia iphone por   dmfjsnfdsnmfdsf sdnfdsfnsdfdfns d sfnsdnfdsnfn c ...",
+                additionalInfo = "Intercambio",
+                location = "Santander de quilichao",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Bolso para dama",
+                description = "Se intercambia iphone por   dmfjsnfdsnmfdsf sdnfdsfnsdfdfns d sfnsdnfdsnfn c ...",
+                additionalInfo = "Intercambio",
+                location = "Jamundi",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Mochila para niño",
+                description = "Se intercambia iphone por   dmfjsnfdsnmfdsf sdnfdsfnsdfdfns d sfnsdnfdsnfn c ...",
+                additionalInfo = "Intercambio",
+                location = "Popayán",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Gorra Goorin Bros",
+                description = "Se intercambia iphone por   dmfjsnfdsnmfdsf sdnfdsfnsdfdfns d sfnsdnfdsnfn c ...",
+                additionalInfo = "Intercambio",
+                location = "Cali",
+                isFavorite = false
+            ),
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Mause inalambrico",
+                description = "Se intercambia iphone por   dmfjsnfdsnmfdsf sdnfdsfnsdfdfns d sfnsdnfdsnfn c ...",
+                additionalInfo = "Intercambio",
+                location = "Cali",
+                isFavorite = false
+            ),
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Estuche para iphone 12 pro max",
+                description = "Se intercambia iphone por   dmfjsnfdsnmfdsf sdnfdsfnsdfdfns d sfnsdnfdsnfn c ...",
+                additionalInfo = "Intercambio",
+                location = "Medellin",
+                isFavorite = false
+            ),
+
+            Item(
+                images = listOf(
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1"),
+                    Uri.parse("android.resource://com.example.givinglandv1/drawable/producto1")
+                ),
+                title = "Camiseta",
                 description = "Se intercambia iphone por ...",
                 additionalInfo = "Donacion",
-                location = "Popayán",
+                location = "Bogota",
                 isFavorite = false
             ),
             // Agrega más elementos aquí...
